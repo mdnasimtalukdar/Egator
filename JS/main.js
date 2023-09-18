d8538a9ca6d28a42960181c084fd6821effbc0a3
Nasim Talukdar <ntn.talukdar@gmail.com>
2023-09-18 15:04:13 +0600

    Add simple JavaScript for faq section and create faq section

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+const faqs = document.querySelectorAll('.faq')
+faqs.forEach(faq => {
+    faq.addEventListener('click',() =>  {
+        faq.classList.toggle('open');
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className ==='uil uil-plus'){
+            icon.className = "uil uil-minus"
+        } else{
+            icon.className = "uil uil-plus"
+        }
+    })
+})
